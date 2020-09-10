47b165bd9498a66566e18c3541b00c9281de5163
Stephen <stephen.germain@free.Fr>
2020-09-10 21:57:07 +0200

    create login

--- src/Controller/AdminSkillController.php
+++ src/Controller/AdminSkillController.php
@@ -50,11 +50,66 @@ class AdminSkillController extends AbstractController
                     'Une erreur est survenue'
                 );
             }
-            return $this->redirectToRoute('adminSkills');
+            return $this->redirectToRoute('admin_skills');
         }
 
         return $this->render('admin/adminForm.html.twig', [
             'formulaire' => $form->createView()
         ]);
+    }
+
+    /**
+     * @Route("/admin/update-{id}", name="skills_update")
+     */
+    public function updateSkills(SkillsRepository $skillsRepository, $id, Request $request){
+
+        $skill = $skillsRepository->find($id);
+
+        $form = $this->createForm(SkillsType::class, $skill);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted()){
+
+            if($form->isValid()){
+                $manager = $this->getDoctrine()->getManager();
+                $manager->persist($skill);
+                $manager->flush();
+                $this->addFlash(
+                    'success',
+                    'La compétences à été modifiée'
+                );
+            }
+            else{
+                $this->addFlash(
+                    'danger',
+                    'Une erreur est survenue'
+                );
+            }
+            return $this->redirectToRoute('admin_skills');
+        }
+
+        return $this->render('admin/adminForm.html.twig', [
+            'formulaire' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/admin/delete-{id}", name="skills_delete")
+     */
+    public function deleteSkill(SkillsRepository $skillsRepository, $id){
+
+        $skill = $skillsRepository->find($id);
+
+        $manager = $this->getDoctrine()->getManager();
+        $manager->remove($skill);
+        $manager->flush();
+
+        $this->addFlash(
+            'danger',
+            'La compétence à bien été supprimée'
+        );
+
+        return $this->redirectToRoute('admin_skills');
+
     }
 }
